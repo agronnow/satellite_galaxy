65d025cb6695964bdbdb94b56c30cb0cb1cd75cd
Asger Groennow <gronnow@astro.rug.nl>
2022-06-13 22:52:37 +0200

    Moved calc_satellite_orbit to class

--- potential/plummer.py
+++ potential/plummer.py
@@ -1,13 +1,14 @@
 import numpy as np
 from .potential import potential
 
+
 class plummer(potential):
-  def __init__(self, M_plummer, r_plummer):
-    self.M_plummer = M_plummer
-    self.r_plummer = r_plummer
+    def __init__(self, M_plummer, r_plummer):
+        self.M_plummer = M_plummer
+        self.r_plummer = r_plummer
 
-  def potential(self, r):
-    return -self.M_plummer/np.sqrt(self.r_plummer**2 + r**2)
+    def potential(self, r):
+        return -self.M_plummer / np.sqrt(self.r_plummer ** 2 + r ** 2)
 
-  def pot_zero(self):
-    return -self.M_plummer/self.r_plummer
+    def pot_zero(self):
+        return -self.M_plummer / self.r_plummer
--- potential/potential.py
+++ potential/potential.py
@@ -2,15 +2,16 @@ import numpy as np
 from scipy.integrate import quad
 from abc import ABC, abstractmethod
 
+
 class potential(ABC):
-  @abstractmethod
-  def potential(self, r):
-     pass
+    @abstractmethod
+    def potential(self, r):
+        pass
 
-  @abstractmethod
-  def pot_zero(self):
-     pass
+    @abstractmethod
+    def pot_zero(self):
+        pass
 
-  def potentialdiff(self, r):
-     #calculate Phi(r) - Phi(0)
-     return self.potential(r) - self.pot_zero()
+    def potentialdiff(self, r):
+        # calculate Phi(r) - Phi(0)
+        return self.potential(r) - self.pot_zero()
